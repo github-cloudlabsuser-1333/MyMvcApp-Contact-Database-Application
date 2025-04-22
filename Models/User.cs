34f3322a357277644f6198ea955ea805da6bf384
github-cloudlabsuser-1333 <168092182+github-cloudlabsuser-1333@users.noreply.github.com>
2025-04-22 17:17:48 +0000

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,16 +1,9 @@
+namespace MyMvcApp.Models;
 
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace MyMvcApp.Models
+public class User
 {
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; } // Add this property
 }
